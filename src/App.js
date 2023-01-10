2eb5b73d9d162eeb1e08007b3b47a29893ae26b9
ugochukwu miracle <ugwumiracle123@gmail.com>
2023-01-11 01:58:32 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState("");
+  const fetchData = async (ipAddress) => {
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_jV2td80d6ABD9qR5Ohp89jAn5hsBa&ipAddress=${ipAddress}`
+    );
+    const data = await response.json();
+    const ip = data.ip;
+    const location = `${data.location.country} ${data.location.region}`;
+    const timeZone = `UTC ${data.location.timezone}`;
+    const isp = data.isp;
+    console.log(ip, location, timeZone, isp);
+    setData(`${ip}, ${location}, ${timeZone}, ${isp}`);
+  };
+
+  fetchData("197.210.226.251");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
     </div>
   );
 }
